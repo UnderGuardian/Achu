6efe726a98001d25846a299f314641d280cf0348
Mark Miller <47294360+UnderGuardian@users.noreply.github.com>
2022-01-31 13:28:02 -0600

    Add files via upload

---
+++ achu.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+public class achu{
+	public static void main(String[] args) {
+		if(java.awt.Desktop.isDesktopSupported()) {
+
+		java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+
+		if(desktop.isSupported(java.awt.Desktop.Action.BROWSE)){
+			try{
+				java.net.URI uri= new java.net.URI("https://www.youtube.com/");
+				desktop.browse(uri);
+			}catch(URISyntaxException | IOException ex){}
+		}
+		}
+	}
+}
